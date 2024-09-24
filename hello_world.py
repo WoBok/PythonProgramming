71308aeb8998f8920c630de41530ddbfa0c6f81d
WoBok <darwin.vinci.wkc@gmail.com>
2024-09-24 15:53:55 +0800

    init

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+message = "Hello World!!"
+print(message)
+titleName = "hello world!"
+titleName = titleName.title()
+print(titleName)
+titleName = titleName.upper()
+print(titleName)
+titleName = titleName.lower()
+print(titleName)
+string1 = "String 1"
+string2 = "String 2"
+fstring = f"拼接两个字符：{string1} {string2}"
+print(fstring)
